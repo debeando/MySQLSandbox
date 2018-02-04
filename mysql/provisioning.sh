cb62e855f5fefecc4aa341f190b9ca9d05801672
Nicola Strappazzon <swapbyt3s@gmail.com>
2018-02-04 20:41:20 +0100

    Amend provision script.

--- mysql/provisioning.sh
+++ mysql/provisioning.sh
@@ -2,20 +2,53 @@
 # encoding: UTF-8
 set -e
 
-ORCHESTRATOR_IP=$(curl --silent localhost:8500/v1/catalog/nodes | jq -r '.[] | select(.Meta.service == "orchestrator") | .Address')
+exec >> /var/log/provisioning.log
+
+PIDFILE=/tmp/provisioning.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 2
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 3
+  fi
+fi
+
 LOCAL_IP=$(awk 'END{print $1}' /etc/hosts)
 
-echo "--> Check whether MySQL is running..."
+echo "--> Local IP Address: ${LOCAL_IP}"
+echo "--> Check MySQL Server in local is running..."
 while ! mysqladmin ping --host=$LOCAL_IP --user="monitor" --password="monitor" --silent > /dev/null 2>&1 ; do
-  sleep 1
+  sleep 10
 done
+
+ORCHESTRATOR_IP=$(curl --silent localhost:8500/v1/catalog/nodes | jq -r '.[] | select(.Meta.service == "orchestrator") | .Address')
+
+echo "--> Orchestrator IP Address: ${ORCHESTRATOR_IP}"
 
 echo "--> Check if registered instance..."
 IS_REGISTERED=$(curl --silent --output /dev/null --write-out '%{http_code}' http://$ORCHESTRATOR_IP:3000/api/instance/$LOCAL_IP/3306)
 if [ $IS_REGISTERED -ne 200 ]
 then
   echo "--> Register new instance..."
-  curl --silent http://$ORCHESTRATOR_IP:3000/api/discover/$LOCAL_IP/3306
+  curl --silent http://$ORCHESTRATOR_IP:3000/api/discover/$LOCAL_IP/3306 > /dev/null 2>&1
   # Wait 10 seconds for internal checks on orchestrator.
   sleep 10
 fi
@@ -24,13 +57,12 @@ MASTER_IP=$(curl --silent http://$ORCHESTRATOR_IP:3000/api/clusters | jq -r 'fir
 SLAVES_IP=($(curl --silent http://$ORCHESTRATOR_IP:3000/api/cluster/$MASTER_IP | jq -r '.[0].SlaveHosts | map(.Hostname) | join(",")'))
 SLAVES_IP=(${SLAVES_IP//,/ })
 
-echo "--> LOCAL_IP: ${LOCAL_IP}"
 echo "--> MASTER_IP: ${MASTER_IP}"
-echo "--> SLAVES_IP: ${SLAVES_IP}"
+echo "--> SLAVES_IP: ${SLAVES_IP[@]}"
 
 if [ -z "$MASTER_IP" ]; then
   echo "--> Is not set variable: MASTER_IP"
-  exit 1
+  exit 4
 fi
 
 IS_MASTER="true"
@@ -45,7 +77,7 @@ then
       if [ "$SLAVE_IP" != "$LOCAL_IP" ]
       then
         SLAVE_STATUS=$(curl --silent http://$ORCHESTRATOR_IP:3000/api/instance/$SLAVE_IP/3306 | jq '(.Slave_SQL_Running == true and .Slave_IO_Running == true)')
-        echo "--> Collect slave status from ${SLAVE_IP}: ${SLAVE_STATUS}"
+        echo "--> Collect status replication from slave: ${SLAVE_IP}"
 
         if [ $SLAVE_STATUS == "true" ]
         then
@@ -57,10 +89,9 @@ then
     done
   fi
 
-  echo "--> Backup data from: ${HOST_IP}"
   if [ $IS_MASTER == "true" ]
   then
-    echo "--> Backup data from master."
+    echo "--> Start backup from master: ${HOST_IP}"
     mysqldump --host=$HOST_IP \
               --user=admin \
               --password=admin \
@@ -68,11 +99,11 @@ then
               --triggers \
               --routines \
               --events \
-              --master-data=2 \
+              --master-data=1 \
               --single-transaction \
               --quick | mysql --force
   else
-    echo "--> Backup data from slave."
+    echo "--> Start backup from slave: ${HOST_IP}"
     mysqldump --host=$HOST_IP \
               --user=admin \
               --password=admin \
@@ -80,22 +111,24 @@ then
               --triggers \
               --routines \
               --events \
-              --dump-slave=2 \
-              --single-transaction \
-              --quick | mysql --force
+              --dump-slave=1 | mysql --force
 
   fi
-  mysqlreplicate --master=admin:admin@$MASTER_IP:3306 \
-                 --slave=admin:admin@127.0.0.1:3306 \
+  echo "--> Setting replication:"
+  mysqlreplicate --master=admin:admin@${MASTER_IP}:3306 \
+                 --slave=admin:admin@${LOCAL_IP}:3306 \
                  --rpl-user=repl:repl \
                  -vvv
 
+  echo "--> Set slave for read only."
   mysql --execute="SET GLOBAL read_only = ON;"
+else
+  echo "--> Is master, ignore backup process."
 fi
 
-if [ -z $(consul kv get -recurse) ]
+if [ -z $(consul kv get -recurse mysql/servers) ]
 then
-  consul kv put mysql/servers $LOCAL_IP
+  consul kv put mysql/servers $LOCAL_IP > /dev/null 2>&1
 else
   VALUES=$(consul kv get mysql/servers)
   VALUES+=",${LOCAL_IP}"
@@ -104,5 +137,8 @@ else
   VALUES=${VALUES[@]}
   VALUES=${VALUES// /,}
 
-  consul kv put mysql/servers $VALUES
+  consul kv put mysql/servers $VALUES > /dev/null 2>&1
 fi
+
+consul kv put mysql/${LOCAL_IP}/logs/provisioning @/var/log/provisioning.log > /dev/null 2>&1
+
